612eb82d29b995596c09bf39a3d2653c6e4cc06a
KaluxikS <salamonskip1@gmail.com>
2024-10-28 20:52:52 +0100

    added requirements and model_training

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from modules import prep_data
+from modules import model_training
 
 def main():
+    # Step 1: Prepeare data
     data_file = "data/games.csv"
     df = prep_data.prepare_data_frame(data_file)
 
-    # Test (safe to delete)
-    print(df)
+    # Step 2: Train the model
+    model, X_test, y_test = model_training.train_model(df)
 
 if __name__ == "__main__":
     main()
