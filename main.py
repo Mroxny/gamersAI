8665a0389265c4c84714f7935bcd75f148219ffc
Artem Yumina <127307970+s25316@users.noreply.github.com>
2024-10-29 02:17:05 +0100

    "add model prediction"

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from modules import prep_data
 from modules import model_training
+from modules import model_prediction
+
 
 def main():
     # Step 1: Prepeare data
@@ -8,6 +10,10 @@ def main():
 
     # Step 2: Train the model
     model, X_test, y_test = model_training.train_model(df)
+
+    # Step 3: Prediction and Results
+    prediction = model_prediction.predict(model, X_test)
+    model_prediction.regression_results(y_test, prediction)
 
 if __name__ == "__main__":
     main()
