b4b9faea6217a5f374a0b57a09f0663838f1bcda
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-11-01 20:17:03 +0100

    Update pipeline

--- src/gamersai/pipelines/data_science/nodes.py
+++ src/gamersai/pipelines/data_science/nodes.py
@@ -2,3 +2,61 @@
 This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.19.9
 """
+import logging
+from typing import Tuple
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+
+
+def split_data(data: pd.DataFrame, parameters: dict) -> Tuple:
+    """Splits data into features and targets training and test sets.
+
+    Args:
+        data: Data containing features and target.
+        parameters: Parameters defined in parameters_data_science.yml.
+    Returns:
+        Split data.
+    """
+
+    X = data[parameters["features"]]
+    y = data["Estimated owners (avg)"]
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=parameters["test_size"], random_state=parameters["random_state"])
+
+    return X_train, X_test, y_train, y_test
+
+
+def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> RandomForestRegressor:
+    """Trains the linear regression model.
+
+    Args:
+        X_train: Training data of independent features.
+        y_train: Training data for price.
+
+    Returns:
+        Trained model.
+    """
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+    return model
+
+
+def evaluate_model(
+    regressor: RandomForestRegressor, X_test: pd.DataFrame, y_test: pd.Series
+):
+    """Calculates and logs the coefficient of determination.
+
+    Args:
+        regressor: Trained model.
+        X_test: Testing data of independent features.
+        y_test: Testing data for price.
+    """
+    y_pred = regressor.predict(X_test)
+    score = r2_score(y_test, y_pred)
+    logger = logging.getLogger(__name__)
+    logger.info("Model has a coefficient R^2 of %.3f on test data.", score)
